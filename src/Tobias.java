915a96a7bcfa506afc2d0ba73d4a29c7942fd793
saibot0506 <tobias.backman@elev.ga.ntig.se>
2022-12-20 15:11:52 +0100

    L

--- src/Tobias.java
+++ src/Tobias.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class Tobias {
+
+    public static String asterisk = new String(new char[word.length()]).replace("\0", "*");
 
     public static boolean validBokstav(String ord, String bokstav){
         if (ord.contains(bokstav))return true;
         else return false;
     }
+
+    
 
 }
