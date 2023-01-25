52a8b1cc8c792323808a4bf66ff001573fecc594
JacobTwentyOne <94972570+JacobTwentyOne@users.noreply.github.com>
2023-01-25 08:55:56 +0100

    Text input och kolla om spelet är slut bra

---
+++ src/jacob.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class jacob {
+ public static void main(String[] args) {
+     String bokstad = JOptionPane.showInputDialog("skriv bonkstad");
+
+// detta tror jag gör så att man skriver in en bokstav och den bokstaven är = stringen bokstad :)
+ }
+public static String title(String ord){
+     if (ord.contains("_"))
+         return "inte klart";
+     return "klart";
+}
+
+
+    }
+
+
+
+
